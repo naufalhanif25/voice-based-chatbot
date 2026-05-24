61629af5edfe4041d65bf49002a306e6d24b8655
naufalhanif25 <naufal.hanif2525@gmail.com>
2026-05-25 00:23:45 +0700

    feat: Update run.sh script

--- run.sh
+++ run.sh
@@ -27,6 +27,20 @@ clean_errors() {
     uv run utils/clean_errors.py
 }
 
+count_errors() {
+    if [ -f "$1" ]; then
+        failed_files=$(jq '[.[] | select(.is_failed == true) | .filename]' "$1")
+        total_failed=$(jq '[.[] | select(.is_failed == true)] | length' "$1")
+        total_data=$(jq '. | length' "$1")
+        failed_percent=$(awk "BEGIN {print ($total_failed / $total_data) * 100}")
+
+        echo "[INFO] Failed files: $failed_files"
+        echo "[INFO] Total 'is_failed: true': $total_failed of $total_data | ~$((total_data - total_failed)) ($failed_percent%) ($1)"
+    else
+        echo "[WARN] File not found: $1 (skipped)"
+    fi
+}
+
 setup_project() {
     if [ -f "pyproject.toml" ]; then
         uv sync
@@ -63,6 +77,8 @@ elif [ "$command" == "clean" ]; then
         clean_logs
         clean_results
     fi
+elif [ "$command" == "sum" ]; then
+    count_errors data/results/checkpoint.json
 else
     echo "Unrecognized command: $command"
 fi
